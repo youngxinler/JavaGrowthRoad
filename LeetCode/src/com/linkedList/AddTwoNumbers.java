28c58301050baf13213a70312442acec2072d03e
YoungXinLer <524022485@qq.com>
2019-06-02 09:14:14 +0800

    addTwoNumbers

--- LeetCode/src/com/linkedList/AddTwoNumbers.java
+++ LeetCode/src/com/linkedList/AddTwoNumbers.java
@@ -7,18 +7,38 @@ package com.linkedList;
 
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        //链表大部分问题都需要记录head
         ListNode res = new ListNode(0);
+        ListNode head = new ListNode(0);
+        head.next = res;
+        //last 记录进位
+        int last = 0;
         while (l1 != null || l2 != null) {
             int x = 0, y = 0;
             if (l1 != null) {
                 x = l1.val;
+                l1 = l1.next;
             }
             if (l2 != null) {
                 y = l2.val;
+                l2 = l2.next;
             }
-            res.next = new ListNode(x + y);
+            int sum = x + y + last;
+            //判断是否大于10, 有进位为1, 没进位为0,
+            if (sum > 9) {
+                last = 1;
+                sum = sum - 10;
+            } else {
+                last = 0;
+            }
+            res.next = new ListNode(sum);
             res = res.next;
         }
-        return res.next;
+        //[9] [9] 这种情况如果最后一位有进位, 最后多添加一个node, 记录进位
+        if (last != 0) {
+            res.next = new ListNode(last);
+        }
+        return head.next.next;
     }
 }
