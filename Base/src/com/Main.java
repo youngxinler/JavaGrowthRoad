85f3ef8933264b8a59e5d375a41e3bfef0278970
YoungXinLer <524022485@qq.com>
2019-07-24 20:37:12 +0800

    update

--- Base/src/com/Main.java
+++ Base/src/com/Main.java
@@ -2,6 +2,8 @@ package com;
 
 import com.myEnum.EnumSingleInstance;
 
+import java.io.IOException;
+
 /**
  * @author youngxinler  19-6-20 下午3:55
  * @version 0.1
@@ -9,9 +11,53 @@ import com.myEnum.EnumSingleInstance;
 
 public class Main {
     public static void main(String[] args) {
-        EnumSingleInstance instance = EnumSingleInstance.SINGLEINSTANCE;
-        instance.method1();
-        instance.method2();
-        instance.displayStr("this is a single instance's display method!");
+//        EnumSingleInstance instance = EnumSingleInstance.SINGLEINSTANCE;
+//        instance.method1();
+//        instance.method2();
+//        instance.displayStr("this is a single instance's display method!");
+
+        Fruit apple = new Apple("apple", 1);
+        try {
+            apple.displayName();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+//        apple.displayName();;
     }
+}
+
+class Fruit{
+    protected String name;
+    protected int number;
+
+    public Fruit(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public void displayName()throws IOException {
+        System.out.println(name);
+    }
+}
+
+class Apple extends Fruit{
+
+    public Apple(String name, int number) {
+        super(name, number);
+    }
+
+    @Override
+    public void displayName(){
+        try {
+            super.displayName();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        System.out.println("son class");
+    }
+
+    public void disPlayNo(){
+        System.out.println(number);
+    }
+
 }
--- LeetCode/src/com/arrayAndStrings/LengthOfLongestSubstring.java
+++ LeetCode/src/com/arrayAndStrings/LengthOfLongestSubstring.java
@@ -9,12 +9,12 @@ public class LengthOfLongestSubstring {
     //滑动窗口
     public int lengthOfLongestSubstring(String s){
         int[] map = new int[128];
-        int ans = 0;
+        int max = 0;
         for (int i = 0, j = 0; i < s.length(); i++) {
             j = Math.max(map[s.charAt(i)], j);
-            ans = Math.max(ans, i - j + 1);
+            max = Math.max(max, i - j + 1);
             map[s.charAt(i)] = i + 1;
         }
-        return ans;
+        return max;
     }
 }
