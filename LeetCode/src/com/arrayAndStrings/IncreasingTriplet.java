6acafefcd1e8044d9c082f6d76e40c0617d82bd2
YoungXinLer <524022485@qq.com>
2019-07-26 11:00:52 +0800

    twice

--- LeetCode/src/com/arrayAndStrings/IncreasingTriplet.java
+++ LeetCode/src/com/arrayAndStrings/IncreasingTriplet.java
@@ -9,17 +9,20 @@ package com.arrayAndStrings;
 public class IncreasingTriplet {
     public boolean increasingTriplet(int[] nums) {
         if (nums.length < 3) return false;
-        int min = Integer.MAX_VALUE, max = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE, mid = Integer.MAX_VALUE;
+        //min是多少无所谓的, min是为了找到mid, 然后只要存在一个数大于mid, 那么说明存在递增的三元子序列.
+        //日后不断更新min是为了舍弃掉不适合的mid, (也可以说是为了mid尽可能地变小)
         for (int num :
                 nums) {
-            if (num > max) {
-                return true;
-            } else if (num < min) {
+            if (num < min){
                 min = num;
-            } else if (num > min && num < max) {
-                max = num;
+            }else if (num < mid && num > min){
+                mid = num;
+            }else {
+                return true;
             }
         }
         return false;
     }
+
 }
