e7132a7f8ea2a64fb60986634da6c340233eb017
YoungXinLer <524022485@qq.com>
2019-08-07 16:49:25 +0800

    twice

--- LeetCode/src/com/backtrack/Exist.java
+++ LeetCode/src/com/backtrack/Exist.java
@@ -5,6 +5,7 @@ package com.backtrack;
  * @version 0.1
  **/
 
+//dfs
 public class Exist {
     public boolean exist(char[][] board, String word) {
         boolean[][] visited = new boolean[board.length][board[0].length];
@@ -21,22 +22,53 @@ public class Exist {
 
     private boolean backTrack(int i, int j, int index, boolean[][] visited, char[][] board, String word) {
         if (index == word.length()) return true;
-        //判断边界, 以及节点是否被重复使用.
         if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || visited[i][j] ||
                 word.charAt(index) != board[i][j]) {
             return false;
         }
-        //防止节点复用.
         visited[i][j] = true;
-        //dfs, 向4个方向探路, 有一个方向true即可.
         if (backTrack(i + 1, j, index + 1, visited, board, word) ||
                 backTrack(i - 1, j, index + 1, visited, board, word)
                 || backTrack(i, j + 1, index + 1, visited, board, word)
                 || backTrack(i, j - 1, index + 1, visited, board, word)) {
             return true;
         }
-        //如果搜索的方向失败, 则恢复节点为false.
         visited[i][j] = false;
         return false;
     }
+
+
+
+
+//    public boolean exist(char[][] board, String word){
+//        boolean[][] visited = new boolean[board.length][board[0].length];
+//        for (int i = 0; i < board.length; i++) {
+//            for (int j = 0; j < board[0].length; j++) {
+//                if (word.charAt(0) == board[i][j] &&
+//                        dfs(board, i, j, 0, word, visited)){
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+//
+//    private boolean dfs(char[][] board, int col, int row, int index, String word, boolean[][] visited){
+//        if (index == word.length()){
+//            return true;
+//        }
+//        if (col < 0 || col >= board[0].length || row < 0 || row >= board.length ||
+//                visited[row][col] || word.charAt(index) != board[row][col]){
+//            return false;
+//        }
+//        visited[row][col] = true;
+//        if (dfs(board, col - 1, row, index + 1, word, visited) ||
+//                dfs(board, col + 1, row, index + 1, word, visited) ||
+//                dfs(board, col, row - 1, index + 1, word, visited) ||
+//                dfs(board, col, row + 1, index + 1, word, visited)){
+//            return true;
+//        }
+//        visited[row][col] = false;
+//        return false;
+//    }
 }
