f68c705df4425d6d326e9ca8d2259f2727948dff
youngxinler <524022485@qq.com>
2019-09-05 19:17:00 +0800

    twice

--- LeetCode/src/com/sortAndSearch/Search.java
+++ LeetCode/src/com/sortAndSearch/Search.java
@@ -6,41 +6,37 @@ package com.sortAndSearch;
  **/
 
 public class Search {
-    private int[] nums;
-    private int target;
-
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) return -1;
         if (nums.length == 1) return nums[0] == target ? 0 : -1;
-        this.nums = nums;
-        this.target = target;
-        int rotateIndex = findRotateIndex(0, nums.length - 1);
+        int rotateIndex = getRotateIndex(nums);
         if (rotateIndex == 0) {
-            return search(0, nums.length - 1);
-        } else if (target == nums[0]) {
+            return binarySearch(nums, 0, nums.length - 1, target);
+        } else if (nums[0] == target) {
             return 0;
         } else if (target > nums[0]) {
-            return search(1, rotateIndex - 1);
+            return binarySearch(nums, 0, rotateIndex - 1, target);
         } else {
-            return search(rotateIndex, nums.length - 1);
+            return binarySearch(nums, rotateIndex, nums.length - 1, target);
         }
     }
 
-    private int search(int l, int r) {
+    private int binarySearch(int[] nums, int l, int r, int target) {
         while (l <= r) {
             int mid = (l + r) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] > target) {
-                r = mid - 1;
-            } else {
+            } else if (target > nums[mid]) {
                 l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
         return -1;
     }
 
-    private int findRotateIndex(int l, int r) {
+    private int getRotateIndex(int[] nums) {
+        int l = 0, r = nums.length - 1;
         if (nums[l] < nums[r]) {
             return 0;
         }
