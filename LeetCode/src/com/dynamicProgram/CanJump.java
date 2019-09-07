578e1fc4d1f46427010add7d0a0214272f4c7e00
youngxinler <524022485@qq.com>
2019-09-07 09:11:27 +0800

    twice

--- LeetCode/src/com/dynamicProgram/CanJump.java
+++ LeetCode/src/com/dynamicProgram/CanJump.java
@@ -22,14 +22,14 @@ public class CanJump {
         return false;
     }
 
-    //贪心
     public boolean canJump(int[] nums) {
-        int lastPos = nums.length - 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (i + nums[i] >= lastPos) {
-                lastPos = i;
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) {
+                return false;
             }
+            max = Math.max(max, i + nums[i]);
         }
-        return lastPos == 0;
+        return true;
     }
 }
