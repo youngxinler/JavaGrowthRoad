552de199333c3b3a6aa9eacc1e235342e2533eb8
youngxinler <524022485@qq.com>
2019-11-10 17:14:29 +0800

    gitignore let my pom.xml dispear

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -1,8 +1,40 @@
-import java.util.Arrays;
+import com.treesAndGraphs.TreeNode;
+
+import java.util.*;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public class Solution {
+        public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+            return selectionSort(input, k);
+        }
+
+        private ArrayList<Integer> selectionSort(int[] array, int k){
+            if(array == null || array.length < k)return null;
+            Integer[] ans = new Integer[k];
+            for(int i = 0; i < k; i++){
+                int min = i;
+                for(int j = i + 1; j < array.length; j++){
+                    if(array[j] < array[min]) min = j;
+                }
+                if(min != i){
+                    int tmp = array[min];
+                    array[min] = array[i];
+                    array[i] = tmp;
+                }
+            }
+            for(int i = 0; i < k; i++){
+                ans[i] = array[i];
+            }
+//            ArrayList<Integer> res = new ArrayList<>(ans.length);
+//            Collections.addAll(res, ans);
+            AbstractQueuedSynchronizer
+            return new ArrayList<Integer>(Arrays.asList(ans));
+
+        }
     }
+
+
 }
 
